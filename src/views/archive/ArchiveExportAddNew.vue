87d0725aff6832cd893a5455631489b26a9cdc10
dongtq <dongtq@imt-soft.com>
2021-02-27 22:48:49 +0700

    + Task 85: Frontend - Develop the export screen

---
+++ src/views/archive/ArchiveExportAddNew.vue
@@ -0,0 +1,313 @@
+<template>
+  <b-container
+    fluid
+    class="d-flex flex-column py-1 px-2"
+  >
+    <!-- START - Header -->
+    <b-row
+      align-v="center"
+      class="justify-content-between mx-0"
+    >
+
+      <!-- START - Back -->
+      <b-row
+        align-v="center"
+        class=""
+      >
+        <b-icon
+          icon="arrow-left"
+          font-scale="2"
+          color="gray"
+        />
+        <b-col class="font-weight-bold text-dark">
+          Tạo phiếu xuất hàng
+        </b-col>
+      </b-row>
+      <!-- END - Back -->
+    </b-row>
+    <!-- END - Header -->
+
+    <!-- START - Form and list -->
+    <b-row no-gutters>
+
+      <!-- START - Form -->
+      <b-col
+        cols="4"
+        class="bg-white shadow rounded mt-4 px-1"
+      >
+        <!-- START - Archive Export Date  -->
+        <b-row class="d-flex my-1">
+          <b-col cols="3">
+            Ngày xuất:
+          </b-col>
+          <b-col class="font-weight-bold">
+            29/10/2020 lúc 16:16
+          </b-col>
+        </b-row>
+        <!-- END - Archive Export Date -->
+
+        <!-- START - Archive Export ID and Type -->
+        <b-form-row>
+          <b-col>
+            <b-form-group
+              label="Mã xuất hàng"
+              label-for="archiveExportID"
+            >
+              <b-form-input
+                id="archiveExportID"
+              />
+            </b-form-group>
+          </b-col>
+
+          <b-col>
+            <b-form-group
+              label="Loại xuất"
+              label-for="archiveExportType"
+            >
+              <b-form-select
+                id="archiveExportType"
+              />
+            </b-form-group>
+          </b-col>
+        </b-form-row>
+        <!-- END - Archive Export ID and Type -->
+
+        <!-- START - Archive Export Archive -->
+        <b-form-group
+          label="Kho hàng"
+          label-for="archiveExportArchive"
+        >
+          <b-form-select
+            id="archiveExportArchive"
+          />
+        </b-form-group>
+        <!-- END - Archive Export Archive -->
+
+        <!-- START - Archive Export Bill Number and Date -->
+        <b-form-row>
+          <b-col>
+            <b-form-group
+              label="Số hóa đơn"
+              label-for="archiveExportBillNumber"
+            >
+              <b-form-input
+                id="archiveExportBillNumber"
+              />
+            </b-form-group>
+          </b-col>
+
+          <b-col>
+            <b-form-group
+              label="Ngày hóa đơn"
+              label-for="archiveExportBillDate"
+            >
+              <b-form-input
+                id="archiveExportBillDate"
+              />
+            </b-form-group>
+          </b-col>
+        </b-form-row>
+        <!-- END - Archive Export Bill Number and Date -->
+
+        <!-- START - Archive Export Internal number and PO no -->
+        <b-form-row>
+          <b-col>
+            <b-form-group
+              label="Số nội bộ"
+              label-for="archiveExportInternalNumber"
+            >
+              <b-form-input
+                id="archiveExportInternalNumber"
+              />
+            </b-form-group>
+          </b-col>
+
+          <b-col>
+            <b-form-group
+              label="PO No"
+              label-for="archiveExportPoNo"
+            >
+              <b-form-input
+                id="archiveExportPoNo"
+              />
+            </b-form-group>
+          </b-col>
+        </b-form-row>
+        <!-- END - Archive Export Internal number and PO no -->
+
+        <!-- START - Archive Export Note -->
+        <b-form-group
+          label="Ghi chú"
+          label-for="customerType"
+        >
+          <b-form-textarea
+            id="customerType"
+          />
+        </b-form-group>
+        <!-- END - Archive Export Note -->
+      </b-col>
+      <!-- END - Form -->
+
+      <!-- START - List -->
+      <b-col class="d-flex flex-column ml-1">
+        <b-row class="ml-2">
+          <div class="bg-white text-primary p-1 rounded-top">
+            Sản phẩm
+          </div>
+          <div class="text-dark ml-1 py-1">
+            Hàng khuyến mãi
+          </div>
+        </b-row>
+        <b-col class="bg-white h-95 rounded">
+          <vue-good-table
+            :columns="columns"
+            :rows="rows"
+            style-class="vgt-table striped"
+            compact-mode
+            line-numbers="true"
+            class="py-1"
+          />
+
+          <!-- START - Button -->
+
+          <b-row class="m-1 justify-content-end">
+            <b-button-group>
+              <b-button
+                variant="primary"
+                size="sm"
+                class="rounded"
+              >
+                <b-icon
+                  icon="download"
+                  width="20"
+                  height="20"
+                  class="mr-1"
+                />
+                Xuất hàng
+              </b-button>
+
+              <b-button
+                size="sm"
+                class="ml-1 rounded"
+              >
+                <b-icon
+                  icon="x"
+                  width="20"
+                  height="20"
+                  class=""
+                />
+                Đóng
+              </b-button>
+            </b-button-group>
+          </b-row>
+          <!-- END - Button -->
+        </b-col>
+
+      </b-col>
+      <!-- END - List -->
+
+    </b-row>
+    <!-- END - Form and list -->
+
+  </b-container>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+      columns: [
+        {
+          label: 'Mã hàng',
+          field: 'ArchiveExportProductID',
+          sortable: false,
+        },
+        {
+          label: 'Số lượng',
+          field: 'ArchiveExportProductAmount',
+          sortable: false,
+        },
+        {
+          label: 'Giá',
+          field: 'ArchiveExportProductPrice',
+          type: 'number',
+          sortable: false,
+        },
+        {
+          label: 'Tên hàng',
+          field: 'ArchiveExportProductName',
+          sortable: false,
+        },
+        {
+          label: 'ĐVT',
+          field: 'ArchiveExportProductDVT',
+          type: 'number',
+          sortable: false,
+        },
+        {
+          label: 'Thành tiền',
+          field: 'ArchiveExportProductMoney',
+          type: 'number',
+          sortable: false,
+        },
+        {
+          label: 'SO No',
+          field: 'ArchiveExportProductSONo',
+          sortable: false,
+        },
+      ],
+      rows: [
+        {
+          ArchiveExportProductID: '23',
+          ArchiveExportProductAmount: '560',
+          ArchiveExportProductPrice: '',
+          ArchiveExportProductName: '',
+          ArchiveExportProductDVT: '',
+          ArchiveExportProductMoney: '25,123,000',
+          ArchiveExportProductSONo: '',
+        },
+        {
+          ArchiveExportProductID: '04AA10',
+          ArchiveExportProductAmount: '12',
+          ArchiveExportProductPrice: '6,300',
+          ArchiveExportProductName: 'STT Dâu ADM GOLD 180ml',
+          ArchiveExportProductDVT: 'Hộp',
+          ArchiveExportProductMoney: '2,531,000',
+          ArchiveExportProductSONo: 'SO.1345',
+        },
+        {
+          ArchiveExportProductID: '04AA10',
+          ArchiveExportProductAmount: '12',
+          ArchiveExportProductPrice: '6,300',
+          ArchiveExportProductName: 'STT Dâu ADM GOLD 180ml',
+          ArchiveExportProductDVT: 'Hộp',
+          ArchiveExportProductMoney: '2,531,000',
+          ArchiveExportProductSONo: 'SO.1345',
+        },
+        {
+          ArchiveExportProductID: '04AA10',
+          ArchiveExportProductAmount: '12',
+          ArchiveExportProductPrice: '6,300',
+          ArchiveExportProductName: 'STT Dâu ADM GOLD 180ml',
+          ArchiveExportProductDVT: 'Hộp',
+          ArchiveExportProductMoney: '2,531,000',
+          ArchiveExportProductSONo: 'SO.1345',
+        },
+        {
+          ArchiveExportProductID: '04AA10',
+          ArchiveExportProductAmount: '12',
+          ArchiveExportProductPrice: '6,300',
+          ArchiveExportProductName: 'STT Dâu ADM GOLD 180ml',
+          ArchiveExportProductDVT: 'Hộp',
+          ArchiveExportProductMoney: '2,531,000',
+          ArchiveExportProductSONo: 'SO.1345',
+        },
+      ],
+    }
+  },
+  methods: {
+  },
+}
+</script>
---
+++ src/views/archive/ArchiveExportList.vue
@@ -0,0 +1,282 @@
+<template>
+  <b-container
+    fluid
+    class="d-flex flex-column"
+  >
+    <!-- START - Search -->
+    <div class="m-1 bg-info shadow-lg rounded">
+      <b-form class="bg-white rounded mt-1">
+        <label
+          for="v-search-form"
+          class="m-1 text-info"
+        >
+          Tìm kiếm
+        </label>
+
+        <b-form-row
+          class="v-search-form border-top p-1"
+        >
+          <b-col cols="2">
+            <b-form-group
+              label="Số hóa đơn"
+              label-for="form-input-customer"
+            >
+              <b-form-input
+                id="form-input-customer"
+                trim
+              />
+            </b-form-group>
+          </b-col>
+
+          <b-col cols="2">
+            <b-form-group
+              class="ml-1"
+              label="Thời gian xuất hàng"
+              label-for="form-input-date"
+            >
+              <b-form-datepicker
+                id="form-input-date"
+                placeholder="chọn từ ngày - đến ngày"
+              />
+            </b-form-group>
+          </b-col>
+
+          <b-col cols="2">
+            <b-form-group
+              class="ml-1"
+              label="Loại xuất"
+              label-for="form-input-customer-group"
+            >
+              <b-form-select
+                id="form-input-customer-group"
+              />
+            </b-form-group>
+          </b-col>
+
+          <b-col>
+            <b-form-group
+              class="ml-1"
+              label="Tìm kiếm"
+              label-for="form-button-search"
+            >
+              <b-button
+                id="form-button-search"
+                variant="info"
+              >
+                <b-icon-search class="mr-1" />
+                Tìm kiếm
+              </b-button>
+            </b-form-group>
+
+          </b-col>
+
+        </b-form-row>
+      </b-form>
+    </div>
+    <!-- END - Search -->
+
+    <!-- START - Customer list -->
+    <div class="v-search mx-1 mb-1 bg-info shadow-lg rounded">
+      <b-form class="bg-white rounded mt-1 pb-1">
+        <b-form-row class="justify-content-between align-items-center border-bottom p-1">
+          <label
+            for=""
+            class="text-info"
+          >
+            Danh sách phiếu xuất hàng
+          </label>
+          <b-button
+            class="rounded"
+            size="md"
+            variant="info"
+          >
+            <b-icon-plus />
+            Thêm mới
+          </b-button>
+        </b-form-row>
+
+        <div class="m-1 mt-1  ">
+          <vue-good-table
+            :columns="columns"
+            :rows="rows"
+            style-class="vgt-table striped"
+            :pagination-options="{
+              enabled: true
+            }"
+            compact-mode
+            line-numbers="true"
+          >
+            <!-- START - label -->
+            <template
+              slot="table-column"
+              slot-scope="props"
+            >
+              <span v-if="props.column.label === 'Chức năng'">
+                <b-icon-bricks />
+              </span>
+              <span v-else>
+                {{ props.column.label }}
+              </span>
+            </template>
+            <!-- END - label -->
+
+            <!-- START - Edit -->
+            <template
+              slot="table-row"
+              slot-scope="props"
+            >
+              <span v-if="props.column.field === 'ArchiveExportFeature'">
+                <b-icon-pencil-fill />
+              </span>
+              <span v-else>
+                {{ props.formattedRow[props.column.field] }}
+              </span>
+            </template>
+          <!-- END - Edit -->
+
+          </vue-good-table>
+        </div>
+      </b-form>
+    </div>
+    <!-- END - Customer list -->
+  </b-container>
+</template>
+
+<script>
+import Vue from 'vue'
+
+import VueGoodTablePlugin from 'vue-good-table'
+// import the styles
+import 'vue-good-table/dist/vue-good-table.css'
+
+Vue.use(VueGoodTablePlugin)
+
+export default {
+  name: 'ArchiveExportList',
+  components: {
+  },
+  data() {
+    return {
+      columns: [
+        {
+          label: 'Ngày',
+          field: 'ArchiveExportDate',
+          sortable: false,
+        },
+        {
+          label: 'Mã xuất hàng',
+          field: 'ArchiveExportID',
+          sortable: false,
+        },
+        {
+          label: 'Số hóa đơn',
+          field: 'ArchiveExportBillNumber',
+          type: 'number',
+          sortable: false,
+        },
+        {
+          label: 'Số nội bộ',
+          field: 'ArchiveExportInternalNumber',
+          sortable: false,
+        },
+        {
+          label: 'Số lượng',
+          field: 'ArchiveExportAmount',
+          type: 'number',
+          sortable: false,
+        },
+        {
+          label: 'Số tiền',
+          field: 'ArchiveExportMoney',
+          type: 'number',
+          sortable: false,
+        },
+        {
+          label: 'Ghi chú',
+          field: 'ArchiveExportNote',
+          sortable: false,
+        },
+        {
+          label: 'Chức năng',
+          field: 'ArchiveExportFeature',
+          sortable: false,
+        },
+      ],
+      rows: [
+        {
+          ArchiveExportDate: '',
+          ArchiveExportID: '',
+          ArchiveExportBillNumber: '',
+          ArchiveExportInternalNumber: '',
+          ArchiveExportAmount: '6800',
+          ArchiveExportMoney: '250.300.000',
+          ArchiveExportNote: '',
+          customerFeature: '',
+        },
+        {
+          ArchiveExportDate: '01/10/2020',
+          ArchiveExportID: 'CA.CH40235.002',
+          ArchiveExportBillNumber: 'IN20201028',
+          ArchiveExportInternalNumber: 'IN.CH40235.001',
+          ArchiveExportAmount: '1020',
+          ArchiveExportMoney: '25.300.000',
+          ArchiveExportNote: '',
+          customerFeature: 'Chỉnh sửa',
+        },
+        {
+          ArchiveExportDate: '01/10/2020',
+          ArchiveExportID: 'CA.CH40235.002',
+          ArchiveExportBillNumber: 'IN20201028',
+          ArchiveExportInternalNumber: 'IN.CH40235.001',
+          ArchiveExportAmount: '1020',
+          ArchiveExportMoney: '25.300.000',
+          ArchiveExportNote: '',
+          customerFeature: 'Chỉnh sửa',
+        },
+        {
+          ArchiveExportDate: '01/10/2020',
+          ArchiveExportID: 'CA.CH40235.002',
+          ArchiveExportBillNumber: 'IN20201028',
+          ArchiveExportInternalNumber: 'IN.CH40235.001',
+          ArchiveExportAmount: '1020',
+          ArchiveExportMoney: '25.300.000',
+          ArchiveExportNote: '',
+          customerFeature: 'Chỉnh sửa',
+        },
+        {
+          ArchiveExportDate: '01/10/2020',
+          ArchiveExportID: 'CA.CH40235.002',
+          ArchiveExportBillNumber: 'IN20201028',
+          ArchiveExportInternalNumber: 'IN.CH40235.001',
+          ArchiveExportAmount: '1020',
+          ArchiveExportMoney: '25.300.000',
+          ArchiveExportNote: '',
+          customerFeature: 'Chỉnh sửa',
+        },
+        {
+          ArchiveExportDate: '01/10/2020',
+          ArchiveExportID: 'CA.CH40235.002',
+          ArchiveExportBillNumber: 'IN20201028',
+          ArchiveExportInternalNumber: 'IN.CH40235.001',
+          ArchiveExportAmount: '1020',
+          ArchiveExportMoney: '25.300.000',
+          ArchiveExportNote: '',
+          customerFeature: 'Chỉnh sửa',
+        },
+        {
+          ArchiveExportDate: '01/10/2020',
+          ArchiveExportID: 'CA.CH40235.002',
+          ArchiveExportBillNumber: 'IN20201028',
+          ArchiveExportInternalNumber: 'IN.CH40235.001',
+          ArchiveExportAmount: '1020',
+          ArchiveExportMoney: '25.300.000',
+          ArchiveExportNote: '',
+          customerFeature: 'Chỉnh sửa',
+        },
+      ],
+    }
+  },
+  methods: {
+  },
+}
+</script>
