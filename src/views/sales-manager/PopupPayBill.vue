b39d89ab7ffc0f7893db134dd54c774947cbcc11
Ty Pham <typham@imt-soft.com>
2021-03-24 14:27:28 +0700

    -Update UI PopupPayBill

--- src/views/sales-manager/PopupPayBill.vue
+++ src/views/sales-manager/PopupPayBill.vue
@@ -27,184 +27,160 @@
         </b-row>
 
         <!-- STart Promotion 1 -->
-        <b-card
-          no-body
+        <b-button
+          block
+          variant="secondary"
+          class="d-flex"
+          @click="onShowPromotion"
         >
-          <b-button
-            v-b-toggle.accordion-1
-            block
-            variant="secondary"
-            class="d-flex"
+          Khuyến mãi ADM tháng 11.2020
+          <b-icon-chevron-down class="ml-auto" />
+        </b-button>
+        <div
+          v-show="ShowPromotion"
+          class="border-secondary"
+        >
+          <vue-good-table
+            :columns="columns"
+            :rows="rows"
+            line-numbers
+            :search-options="{
+              enabled: true,
+              externalQuery: searchTerm
+            }"
           >
-            Khuyến mãi ADM tháng 11.2020
-            <b-icon-chevron-down class="ml-auto" />
-          </b-button>
-          <b-collapse
-            id="accordion-1"
-            accordion="my-accordion"
-          >
-
-            <!-- Start table -->
-            <vue-good-table
-              :columns="columns"
-              :rows="rows"
-              line-numbers
-              :search-options="{
-                enabled: true,
-                externalQuery: searchTerm
-              }"
+            <template
+              slot="table-column"
+              slot-scope="props"
             >
-              <template
-                slot="table-column"
-                slot-scope="props"
-              >
-                <div
-                  v-if="props.column.label === 'Chức năng'"
-                />
-                <div v-else>
-                  {{ props.column.label }}
-                </div>
-              </template>
+              <div
+                v-if="props.column.label === 'Chức năng'"
+              />
+              <div v-else>
+                {{ props.column.label }}
+              </div>
+            </template>
 
-              <template
-                slot="table-row"
-                slot-scope="props"
-              >
-                <div v-if="props.column.field === 'Press'">
-                  <b-button
-                    variant="secondary"
-                    @click="onPress"
-                  >
-                    <b-icon-hand-index-thumb />
-                    CHỌN ĐƠN
-                  </b-button>
-                </div>
-              </template>
-            </vue-good-table>
-            <!-- End table -->
-
-          </b-collapse>
-        </b-card>
+            <template
+              slot="table-row"
+              slot-scope="props"
+            >
+              <div v-if="props.column.field === 'Press'">
+                <b-button
+                  variant="secondary"
+                  @click="onPress"
+                >
+                  <b-icon-hand-index-thumb />
+                  CHỌN ĐƠN
+                </b-button>
+              </div>
+            </template>
+          </vue-good-table>
+        </div>
         <!-- End Promotion 1 -->
 
         <!-- STart Promotion 2 -->
-        <b-card
-          no-body
+        <b-button
+          block
+          variant="secondary"
+          class="d-flex mt-1"
+          @click="onShowProductPromotion"
         >
-          <b-button
-            v-b-toggle.accordion-2
-            block
-            variant="secondary"
-            class="d-flex"
+          Khuyến mãi tay PRM.001 - tặng sản phẩm
+          <b-icon-chevron-down class="ml-auto" />
+        </b-button>
+        <div
+          v-show="ShowProductPromotion"
+          class="border-secondary"
+        >
+          <vue-good-table
+            :columns="columns2"
+            :rows="rows2"
+            line-numbers
+            :search-options="{
+              enabled: true,
+              externalQuery: searchTerm
+            }"
           >
-            Khuyến mãi tay PRM.001 - tặng sản phẩm
-            <b-icon-chevron-down class="ml-auto" />
-          </b-button>
-          <b-collapse
-            id="accordion-2"
-            accordion="my-accordion"
-          >
-
-            <!-- Start table -->
-            <vue-good-table
-              :columns="columns2"
-              :rows="rows2"
-              line-numbers
-              :search-options="{
-                enabled: true,
-                externalQuery: searchTerm
-              }"
+            <template
+              slot="table-row"
+              slot-scope="props"
             >
-              <template
-                slot="table-row"
-                slot-scope="props"
-              >
-                <!-- <div v-if="props.column.field === 'Input2'">
+              <!-- <div v-if="props.column.field === 'Input2'">
                   <b-form-input
                     id="form-input-customer"
                     class="w-75"
                   />
                 </div> -->
-                <!-- <div v-if="props.column.field === 'ProgramName'">
+              <!-- <div v-if="props.column.field === 'ProgramName'">
                   <b-form-input
                     id="form-input-customer"
                   />
                 </div> -->
-                <div v-if="props.column.field === 'Press'">
-                  <b-form-input
-                    id="form-input-customer"
-                    v-model="totalMoney"
-                    disabled
-                    class="w-75"
-                  />
-                </div>
-              </template>
-            </vue-good-table>
-            <!-- End table -->
+              <div v-if="props.column.field === 'Press'">
+                <b-form-input
+                  id="form-input-customer"
+                  v-model="totalMoney"
+                  disabled
+                  class="w-75"
+                />
+              </div>
+            </template>
+          </vue-good-table>
+        </div>
 
-          </b-collapse>
-        </b-card>
         <!-- End Promotion 2 -->
 
         <!-- STart Promotion 3 -->
-        <b-card
-          no-body
+        <b-button
+          block
+          variant="secondary"
+          class="d-flex mt-1"
+          @click="onShowMoneyPromotion"
         >
-          <b-button
-            v-b-toggle.accordion-3
-            block
-            variant="secondary"
-            class="d-flex"
-          >
-            Khuyến mãi tay PRM.001 - tặng tiền
-            <b-icon-chevron-down class="ml-auto" />
-          </b-button>
-          <b-collapse
-            id="accordion-3"
-            accordion="my-accordion"
-          >
-            <b-row class="p-2">
-              <b-col>
-                Số tiền
-              </b-col>
-              <b-col>
-                <b-form-input
-                  id="form-input-customer"
-                />
-              </b-col>
-            </b-row>
-
-          </b-collapse>
-        </b-card>
+          Khuyến mãi tay PRM.001 - tặng tiền
+          <b-icon-chevron-down class="ml-auto" />
+        </b-button>
+        <div
+          v-show="ShowMoneyPromotion"
+          class="border-secondary"
+        >
+          <b-row class="p-2">
+            <b-col>
+              Số tiền
+            </b-col>
+            <b-col>
+              <b-form-input
+                id="form-input-customer"
+              />
+            </b-col>
+          </b-row>
+        </div>
         <!-- End Promotion 3 -->
 
         <!-- STart Promotion 4 -->
-        <b-card
-          no-body
+        <b-button
+          block
+          variant="secondary"
+          class="d-flex mt-1"
+          @click="onShowPresent"
         >
-          <b-button
-            v-b-toggle.accordion-4
-            block
-            variant="secondary"
-            class="d-flex"
-          >
-            Tích lũy - Quà Tết 2021
-            <b-icon-chevron-down class="ml-auto" />
-          </b-button>
-          <b-collapse
-            id="accordion-4"
-            accordion="my-accordion"
-          >
-            <b-row class="p-2">
-              <b-col>
-                Quà tặng
-              </b-col>
-              <b-col>
-                01 bộ tô chén dĩa Minh Long
-              </b-col>
-            </b-row>
-          </b-collapse>
-        </b-card>
+          Tích lũy - Quà Tết 2021
+          <b-icon-chevron-down class="ml-auto" />
+        </b-button>
+        <div
+          v-show="ShowPresent"
+          class="py-2 border-secondary"
+        >
+          <b-row>
+            <b-col>
+              Quà tặng
+            </b-col>
+            <b-col>
+              01 bộ tô chén dĩa Minh Long
+            </b-col>
+          </b-row>
+        </div>
         <!-- End Promotion 4 -->
 
       </b-col>
@@ -226,6 +202,7 @@
           </h2>
         </b-row>
 
+        <!-- Start Total Money -->
         <b-row class="mt-1">
           <b-col
             xl
@@ -243,10 +220,12 @@
               id="form-input-customer"
               v-model="totalMoney"
               disabled
+              class="text-right"
             />
           </b-col>
         </b-row>
 
+        <!-- Start Discount -->
         <b-row class="mt-1">
           <b-col
             xl
@@ -263,10 +242,12 @@
             <b-form-input
               id="form-input-customer"
               v-model="discount"
+              class="text-right"
             />
           </b-col>
         </b-row>
 
+        <!-- Start Cumulative Points -->
         <b-row class="mt-1">
           <b-col
             xl
@@ -280,13 +261,37 @@
             sm="4"
             md="3"
           >
-            <b-form-input
-              id="form-input-customer"
-              v-model="points"
-            />
+            <b-row>
+              <b-col>
+                <b-form-input
+                  id="input-live"
+                  v-model="points"
+                  class="text-center"
+                  maxlength="20"
+                  disabled
+                  @click="error"
+                />
+              </b-col>
+              <b-col>
+                <b-input-group class="input-group-merge">
+
+                  <b-input-group-addon is-text>
+                    <b-form-checkbox value="accepted" />
+                  </b-input-group-addon>
+                  <b-form-input
+                    id="form-input-customer"
+                    v-model="pointsMoney"
+                    maxlength="20"
+                    class="text-right"
+                  />
+                </b-input-group>
+              </b-col>
+            </b-row>
+
           </b-col>
         </b-row>
 
+        <!-- Start Voucher -->
         <b-row class="mt-1">
           <b-col
             xl
@@ -300,13 +305,37 @@
             sm="4"
             md="3"
           >
-            <b-form-input
-              id="form-input-customer"
-              v-model="voucher"
-            />
+            <b-row>
+              <b-col>
+                <b-input-group class="input-group-merge">
+                  <b-input-group-prepend is-text>
+                    <b-icon-three-dots-vertical />
+                  </b-input-group-prepend>
+                  <b-form-input
+                    id="input-live"
+                    v-model="voucherCode"
+                    class="text-center"
+                    maxlength="20"
+                    @click="error"
+                  />
+                  <b-input-group-append is-text>
+                    <b-icon-x />
+                  </b-input-group-append>
+                </b-input-group>
+              </b-col>
+              <b-col>
+                <b-form-input
+                  v-model="voucherMoney"
+                  class="text-right"
+                  disabled
+                />
+              </b-col>
+            </b-row>
+
           </b-col>
         </b-row>
 
+        <!-- Start Discount Code -->
         <b-row class="mt-1">
           <b-col
             xl
@@ -320,13 +349,34 @@
             sm="4"
             md="3"
           >
-            <b-form-input
-              id="form-input-customer"
-              v-model="discountCode"
-            />
+            <b-row>
+              <b-col>
+                <b-input-group class="input-group-merge">
+                  <b-form-input
+                    id="input-live"
+                    v-model="discountCode"
+                    class="text-center"
+                    maxlength="20"
+                    @click="error"
+                  />
+                  <b-input-group-append is-text>
+                    <b-icon-x />
+                  </b-input-group-append>
+                </b-input-group>
+              </b-col>
+              <b-col>
+                <b-form-input
+                  id="form-input-customer"
+                  v-model="discountMoney"
+                  class="text-right"
+                  disabled
+                />
+              </b-col>
+            </b-row>
           </b-col>
         </b-row>
 
+        <!-- Start Customer Pay -->
         <b-row class="mt-1">
           <b-col
             xl
@@ -343,11 +393,13 @@
             <b-form-input
               id="form-input-customer"
               v-model="customerPay"
+              class="text-right font-weight-bolder"
               disabled
             />
           </b-col>
         </b-row>
 
+        <!-- Start Payment Customers -->
         <b-row class="mt-1">
           <b-col
             xl
@@ -361,12 +413,29 @@
             sm="4"
             md="3"
           >
-            <b-form-input
-              id="form-input-customer"
-            />
+            <b-input-group>
+              <b-input-group-prepend>
+                <b-form-select
+                  v-model="selected"
+                  :options="options"
+                  value-field="item"
+                  text-field="name"
+                />
+              </b-input-group-prepend>
+
+              <b-form-input
+                id="input-live"
+                v-model="paymentCustomers"
+                maxlength="20"
+                class="text-right"
+                @click="error"
+              />
+            </b-input-group>
           </b-col>
+
         </b-row>
 
+        <!-- Start Excess Cash -->
         <b-row class="mt-1">
           <b-col
             xl
@@ -384,10 +453,10 @@
               id="form-input-customer"
               v-model="excessCash"
               disabled
+              class="text-right"
             />
           </b-col>
         </b-row>
-
       </b-col>
       <!-- End Container  Right -->
     </b-row>
@@ -396,7 +465,7 @@
     <!-- Start Button Bottom  -->
     <b-row
       align-h="center"
-      class="mt-4"
+      class="my-4"
     >
       <b-button
         class="mr-2"
@@ -458,15 +527,30 @@ export default {
   },
   data() {
     return {
+
+      // Show
+      ShowPromotion: true,
+      ShowProductPromotion: true,
+      ShowMoneyPromotion: true,
+      ShowPresent: true,
+
       totalMoney: '1,160,000',
       discount: '30,000',
       points: '600 điểm',
-      voucher: '400,000',
-      discountCode: '200,000',
+      pointsMoney: '60,000',
+      voucherCode: 'TET.400K',
+      voucherMoney: '400,000',
+      discountCode: 'SN.20%',
+      discountMoney: '200,000',
       customerPay: '416,000',
       excessCash: '83,951',
+      paymentCustomers: '500,000',
 
       searchTerm: '',
+      selected: '1',
+      options: [
+        { item: '1', name: 'Tiền mặt' },
+      ],
       columns: [
         {
           label: 'Mã sản phẩm',
@@ -532,6 +616,34 @@ export default {
   },
   methods: {
     onPress() {
+    },
+    onShowPromotion() {
+      if (this.ShowPromotion === false) {
+        this.ShowPromotion = true
+      } else {
+        this.ShowPromotion = false
+      }
+    },
+    onShowProductPromotion() {
+      if (this.ShowProductPromotion === false) {
+        this.ShowProductPromotion = true
+      } else {
+        this.ShowProductPromotion = false
+      }
+    },
+    onShowMoneyPromotion() {
+      if (this.ShowMoneyPromotion === false) {
+        this.ShowMoneyPromotion = true
+      } else {
+        this.ShowMoneyPromotion = false
+      }
+    },
+    onShowPresent() {
+      if (this.ShowPresent === false) {
+        this.ShowPresent = true
+      } else {
+        this.ShowPresent = false
+      }
     },
   },
 }
