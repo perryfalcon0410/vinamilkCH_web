eae9990afa7e19563a87707e6262b23db73d54d3
Dong Trinh <dongtq@imt-soft.com>
2021-04-03 10:41:27 +0700

    + NEW [Frontend] - Change Damaged Goods List and Add new

--- src/views/warehouses/warehouses-exchange-damaged-goods/warehouses-exchange-damaged-goods-create/WarehousesExchangeDamagedGoodsCreate.vue
+++ src/views/warehouses/warehouses-exchange-damaged-goods/warehouses-exchange-damaged-goods-create/WarehousesExchangeDamagedGoodsCreate.vue
@@ -1,15 +1,346 @@
 <template>
-  <div class="p-4">
-    <h4 class="mb-2">
-      Warehouse Exchange Damaged Goods Create
-    </h4>
-  </div>
+  <b-container
+    fluid
+    class="pb-1 px-0"
+  >
+    <!-- START - Form and list -->
+    <b-row class="mx-0">
+      <!-- START - Form -->
+      <b-col
+        xl="3"
+        class="mr-xl-1 bg-white shadow rounded"
+      >
+        <!-- START -  Date  -->
+        <b-row class="my-1">
+          <b-col>
+            Ngày biên bản:
+          </b-col>
+          <div class="font-weight-bold mx-1">
+            29/10/2020 lúc 16:16
+          </div>
+        </b-row>
+        <!-- END -  Date -->
+
+        <!-- START -  Number -->
+        <b-form-group
+          :state="stateInputID"
+          invalid-feedback="Chỉ bao gồm các ký tự [0-9], [a-Z], dấu chấm(.), dấu gạch dưới (_)"
+        >
+          Số phiên bản <sup class="text-danger">*</sup>
+          <b-form-input
+            id="archiveExportID"
+            v-model="inputValueID"
+            maxlength="40"
+            :state="stateInputID"
+            required
+            trim
+          />
+        </b-form-group>
+        <!-- END -  Number -->
+
+        <!-- START -  Customer -->
+        <b-form-group>
+          Khách hàng <sup class="text-danger">*</sup>
+          <b-form-select
+            id="archiveExportArchive"
+          />
+        </b-form-group>
+        <!-- END -  Customer -->
+
+        <!-- START -  Address-->
+        <b-form-group>
+          Địa chỉ <sup class="text-danger">*</sup>
+
+          <b-form-input
+            id="archiveExportBillNumber"
+            v-model="inputValueBillNumber"
+            required
+          />
+        </b-form-group>
+        <!-- END -  Address-->
+
+        <!-- START -  Phone number -->
+        <b-form-group
+          :state="stateInputInternalNumber"
+          invalid-feedback="Chỉ bao gồm ký tự [0-9]"
+        >
+          Di động <sup class="text-danger">*</sup>
+          <b-form-input
+            id="archiveExportInternalNumber"
+            v-model="inputValueInternalNumber"
+            maxlength="20"
+            :state="stateInputInternalNumber"
+            required
+          />
+        </b-form-group>
+        <!-- END -  Phone number -->
+
+        <!-- START -  Reason -->
+        <b-form-group>
+          Lý do <sup class="text-danger">*</sup>
+          <b-form-select
+            id="customerType"
+          />
+        </b-form-group>
+        <!-- END -  Reason -->
+
+      </b-col>
+      <!-- END - Form -->
+
+      <!-- START - List -->
+      <b-col class="bg-white shadow rounded mt-1 mt-xl-0 p-1">
+        <!-- START - Table Product -->
+        <vue-good-table
+          :columns="columns"
+          :rows="rowsProduct"
+          style-class="vgt-table striped"
+          compact-mode
+          line-numbers
+        >
+          <!-- START - Column -->
+          <template
+            slot="table-column"
+            slot-scope="props"
+          >
+            <div v-if="props.column.field === 'Feature'">
+              <b-icon-bricks />
+            </div>
+            <div v-else>
+              {{ props.column.label }}
+            </div>
+          </template>
+          <!-- END - Column -->
+
+          <!-- START - Row -->
+          <template
+            slot="table-row"
+            slot-scope="props"
+          >
+            <div v-if="props.column.field === 'Feature'">
+              <b-icon-trash-fill color="red" />
+            </div>
+
+            <div v-else>
+              {{ props.formattedRow[props.column.field] }}
+            </div>
+          </template>
+          <!-- END - Row -->
+
+          <template slot="table-actions-bottom">
+            <b-col
+              cols="5"
+              class="m-1"
+            >
+              <b-input placeholder="Nhập mã hoặc tên sản phẩm" />
+            </b-col>
+          </template>
+
+          <!-- START - Filter -->
+          <template
+            slot="column-filter"
+            slot-scope="props"
+          >
+            <div
+              v-if="props.column.field === 'ProductCode'"
+            >
+              23 dòng
+            </div>
+            <div
+              v-else-if="props.column.field === 'ProductAmount'"
+            >
+              560
+            </div>
+            <div
+              v-else-if="props.column.field === 'ProductPriceTotal'"
+              class="float-right"
+            >
+              25,123,000
+            </div>
+          </template>
+          <!-- END - Filter -->
+
+        </vue-good-table>
+        <!-- END - Table Product -->
+
+        <!-- START - Button -->
+        <b-row class="m-1 justify-content-end">
+          <b-button-group>
+            <b-button
+              variant="primary"
+              size="sm"
+              class="rounded text-uppercase"
+            >
+              <b-icon
+                icon="download"
+                width="20"
+                height="20"
+                class="mr-1"
+              />
+              Trả hàng
+            </b-button>
+
+            <b-button
+              size="sm"
+              class="ml-1 rounded text-uppercase"
+              @click="routeBack"
+            >
+              <b-icon-x
+                width="20"
+                height="20"
+              />
+              Đóng
+            </b-button>
+          </b-button-group>
+        </b-row>
+        <!-- END - Button -->
+      </b-col>
+      <!-- END - List -->
+    </b-row>
+    <!-- END - Form and list -->
+
+  </b-container>
 </template>
 
 <script>
+import { VueGoodTable } from 'vue-good-table'
+// import the styles
+import 'vue-good-table/dist/vue-good-table.css'
 
 export default {
   components: {
+    VueGoodTable,
+
+  },
+  data() {
+    return {
+      selected: 'phiếu nhập',
+      EntryAdjustmentModalVisible: false,
+      EntryBorrowedModalVisible: false,
+      EntryCouponModalVisible: false,
+      inputValueID: '',
+      inputValueBillNumber: '',
+      inputValueInternalNumber: '',
+      inputValueDate: new Date(),
+      columns: [
+        {
+          label: 'Mã hàng',
+          field: 'ProductCode',
+          sortable: false,
+          filterOptions: {
+            enabled: true,
+          },
+        },
+        {
+          label: 'Tên hàng',
+          field: 'ProductName',
+          sortable: false,
+        },
+        {
+          label: 'ĐVT',
+          field: 'ProductDVT',
+          type: 'number',
+          sortable: false,
+        },
+        {
+          label: 'Giá',
+          field: 'ProductPrice',
+          type: 'number',
+          sortable: false,
+        },
+        {
+          label: 'Số lượng ',
+          field: 'ProductAmount',
+          sortable: false,
+          filterOptions: {
+            enabled: true,
+          },
+        },
+        {
+          label: 'Thành tiền',
+          field: 'ProductPriceTotal',
+          type: 'number',
+          sortable: false,
+          filterOptions: {
+            enabled: true,
+          },
+        },
+        {
+          label: 'Thao tác',
+          field: 'Feature',
+          sortable: false,
+        },
+      ],
+      rowsProduct: [
+        {
+          ProductCode: '04AA10',
+          ProductName: 'STT Dâu ADM GOLD 180ml',
+          ProductDVT: 'Hộp',
+          ProductPrice: '6,300',
+          ProductAmount: '12',
+          ProductPriceTotal: '2,531,000',
+          Feature: '',
+        },
+        {
+          ProductCode: '04AA10',
+          ProductName: 'STT Dâu ADM GOLD 180ml',
+          ProductDVT: 'Hộp',
+          ProductPrice: '6,300',
+          ProductAmount: '12',
+          ProductPriceTotal: '2,531,000',
+          Feature: '',
+        },
+        {
+          ProductCode: '04AA10',
+          ProductName: 'STT Dâu ADM GOLD 180ml',
+          ProductDVT: 'Hộp',
+          ProductPrice: '6,300',
+          ProductAmount: '12',
+          ProductPriceTotal: '2,531,000',
+          Feature: '',
+        },
+      ],
+    }
+  },
+
+  computed: {
+    stateInputID() {
+      const validID = /^([\w\\.]{0,40})$/
+      const result = validID.test(this.inputValueID)
+      if (this.inputValueID.length >= 1) {
+        return result
+      }
+      return null
+    },
+    stateInputBillNumber() {
+      const validBillNumber = /^(\d{0,20})$/
+      const result = validBillNumber.test(this.inputValueBillNumber)
+      if (this.inputValueBillNumber.length >= 1) {
+        return result
+      }
+      return null
+    },
+    stateInputInternalNumber() {
+      const validInternalNumber = /^(\d{0,20})$/
+      const result = validInternalNumber.test(this.inputValueInternalNumber)
+      if (this.inputValueInternalNumber.length >= 1) {
+        return result
+      }
+      return null
+    },
+  },
+
+  methods: {
+    routeBack() {
+      this.$router.back()
+    },
+
+    showModal() {
+      const ShowEntryCouponModal = this.selected === 'phiếu nhập' ? this.EntryCouponModalVisible = !this.EntryCouponModalVisible : this.EntryCouponModalVisible = false
+      const ShowEntryAdjustmentModal = this.selected === 'phiếu điều chỉnh' ? this.EntryAdjustmentModalVisible = !this.EntryAdjustmentModalVisible : this.EntryAdjustmentModalVisible = false
+      const ShowEntryBorrowedModal = this.selected === 'phiếu vay mượn' ? this.EntryBorrowedModalVisible = !this.EntryBorrowedModalVisible : this.EntryBorrowedModalVisible = false
+
+      return ShowEntryCouponModal && ShowEntryAdjustmentModal && ShowEntryBorrowedModal
+    },
   },
 }
 </script>
--- src/views/warehouses/warehouses-exchange-damaged-goods/warehouses-exchange-damaged-goods-list/WarehousesExchangeDamagedGoodsList.vue
+++ src/views/warehouses/warehouses-exchange-damaged-goods/warehouses-exchange-damaged-goods-list/WarehousesExchangeDamagedGoodsList.vue
@@ -1,15 +1,337 @@
 <template>
-  <div class="p-4">
-    <h4 class="mb-2">
-      Warehouse Exchange Damaged Goods List
-    </h4>
-  </div>
+  <b-container
+    fluid
+    class=" p-0"
+  >
+    <!-- START - Search -->
+    <b-form class="bg-white shadow rounded">
+      <!-- START - Label -->
+      <label
+        for="v-search-form"
+        class="m-1 text-primary"
+      >
+        Tìm kiếm
+      </label>
+      <!-- END - Label -->
+
+      <!-- START - Section form input -->
+      <b-form-row
+        class="v-search-form border-top p-1"
+      >
+        <!-- START - Minute Code -->
+        <b-col
+          lg="2"
+          md
+        >
+          <b-form-group
+            label="Số biên bản"
+            label-for="form-input-minuteCode"
+            :state="validatorCode"
+            invalid-feedback="Chỉ bao gồm các ký tự [0-9][a-Z] dấu chấm(.), dấu gạch dưới (_)"
+          >
+            <b-form-input
+              id="form-input-minuteCode"
+              v-model="MinuteCode"
+              maxlength="20"
+              :state="validatorCode"
+              required
+              trim
+            />
+          </b-form-group>
+        </b-col>
+        <!-- END - Minute Code -->
+
+        <!-- START - Date to -->
+        <b-col
+          lg="2"
+          md
+        >
+          <b-form-group
+            class="ml-lg-1"
+            label="Từ ngày"
+            label-for="form-input-date-from"
+          >
+            <b-form-datepicker
+              id="form-input-date-from"
+              v-model="fromDate"
+              :date-format-options="{day: '2-digit', month: '2-digit', year: 'numeric'}"
+              locale="vi"
+            />
+          </b-form-group>
+        </b-col>
+        <!-- END - Date to -->
+
+        <!-- START - Date from -->
+        <b-col
+          lg="2"
+          md
+        >
+          <b-form-group
+            class="ml-lg-1"
+            label="Đến ngày"
+            label-for="form-input-date-to"
+          >
+            <b-form-datepicker
+              id="form-input-date-to"
+              v-model="toDate"
+              :date-format-options="{day: '2-digit', month: '2-digit', year: 'numeric'}"
+              locale="vi"
+            />
+          </b-form-group>
+        </b-col>
+        <!-- END - Date from -->
+
+        <!-- START - Reason -->
+        <b-col
+          lg="2"
+          md
+        >
+          <b-form-group
+            class="ml-lg-1"
+            label="Lý do"
+            label-for="form-select-reason"
+          >
+            <b-form-select
+              id="form-select-reason"
+            />
+          </b-form-group>
+        </b-col>
+        <!-- END - Reason -->
+
+        <!-- START - Search Button -->
+        <b-col
+          md="12"
+          lg="4"
+        >
+          <b-form-group
+            class="ml-lg-1"
+            label="Tìm kiếm"
+            label-for="form-button-search"
+          >
+            <b-button
+              id="form-button-search"
+              variant="primary"
+            >
+              <b-icon-search class="mr-1" />
+              Tìm kiếm
+            </b-button>
+          </b-form-group>
+        </b-col>
+        <!-- END - Search Button -->
+
+      </b-form-row>
+      <!-- END - Section form input -->
+
+    </b-form>
+    <!-- END - Search -->
+
+    <!-- START - Product Import list -->
+    <b-form class="bg-white rounded shadow my-1">
+      <!-- START - Title -->
+      <b-form-row class="justify-content-between align-items-center border-bottom p-1">
+        <label
+          for="listProduct"
+          class="text-primary"
+        >
+          Danh sách phiếu đổi hàng hỏng
+        </label>
+
+        <b-button
+          class="rounded"
+          size="sm"
+          variant="primary"
+          @click="onClickAddNewButton"
+        >
+          <b-icon-plus scale="1.5" />
+          Thêm mới
+        </b-button>
+      </b-form-row>
+      <!-- END - Title -->
+
+      <!-- START - Table -->
+      <b-col
+        id="listProduct"
+        class="py-1"
+      >
+        <vue-good-table
+          :columns="columns"
+          :rows="rows"
+          style-class="vgt-table striped"
+          :pagination-options="{
+            enabled: true
+          }"
+          compact-mode
+          line-numbers
+        >
+          <!-- START - Column -->
+          <template
+            slot="table-column"
+            slot-scope="props"
+          >
+            <b-row
+              v-if="props.column.field === 'Feature'"
+              align-h="center"
+            >
+              <b-icon-bricks />
+            </b-row>
+            <div v-else>
+              {{ props.column.label }}
+            </div>
+          </template>
+          <!-- END - Column -->
+
+          <!-- START - Row -->
+          <template
+            slot="table-row"
+            slot-scope="props"
+          >
+            <b-row
+              v-if="props.column.field === 'Feature'"
+              class="mx-0"
+              align-h="around"
+            >
+              <b-icon-pencil-fill
+                class="cursor-pointer"
+                @click="onClickUpdateButton"
+              />
+
+              <b-icon-trash-fill
+                class="cursor-pointer"
+                color="red"
+              />
+            </b-row>
+
+            <div v-else>
+              {{ props.formattedRow[props.column.field] }}
+            </div>
+          </template>
+          <!-- END - Row -->
+
+          <!-- START - Filter -->
+          <template
+            slot="column-filter"
+            slot-scope="props"
+          >
+            <div
+              v-if="props.column.field === 'Amount'"
+            >
+              0
+            </div>
+            <div
+              v-else-if="props.column.field === 'Price'"
+            >
+              0
+            </div>
+          </template>
+          <!-- END - Filter -->
+
+        </vue-good-table>
+      </b-col>
+      <!-- END - Table -->
+
+    </b-form>
+    <!-- END - Product Import list -->
+
+  </b-container>
 </template>
 
 <script>
+import { VueGoodTable } from 'vue-good-table'
+import 'vue-good-table/dist/vue-good-table.css'
 
 export default {
   components: {
+    VueGoodTable,
+  },
+  data() {
+    return {
+      fromDate: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+      toDate: new Date(),
+      MinuteCode: '',
+      columns: [
+        {
+          label: 'Ngày',
+          field: 'Date',
+          sortable: false,
+        },
+        {
+          label: 'Số biên bản',
+          field: 'MinuteCode',
+          sortable: false,
+        },
+        {
+          label: 'Số lượng',
+          field: 'Amount',
+          sortable: false,
+          filterOptions: {
+            enabled: true,
+          },
+        },
+        {
+          label: 'Số tiền',
+          field: 'Price',
+          sortable: false,
+          filterOptions: {
+            enabled: true,
+          },
+        },
+        {
+          label: 'Lý do',
+          field: 'Reason',
+          sortable: false,
+        },
+        {
+          label: 'Chức năng',
+          field: 'Feature',
+          sortable: false,
+          width: '100px',
+        },
+      ],
+      rows: [
+        {
+          Date: '12/03/2021',
+          MinuteCode: 'CA.CH40235.002',
+          Amount: '1020',
+          Price: '25.300.000',
+          Reason: 'Hàng hỏng do khách hàng mua tại cửa hàng',
+          Feature: '',
+        },
+        {
+          Date: '12/03/2021',
+          MinuteCode: 'CA.CH40235.002',
+          Amount: '1020',
+          Price: '25.300.000',
+          Reason: 'Hàng hỏng do khách hàng mua tại cửa hàng',
+          Feature: '',
+        },
+        {
+          Date: '12/03/2021',
+          MinuteCode: 'CA.CH40235.002',
+          Amount: '1020',
+          Price: '25.300.000',
+          Reason: 'Hàng hỏng do khách hàng mua tại cửa hàng',
+          Feature: '',
+        },
+      ],
+    }
+  },
+  computed: {
+    validatorCode() {
+      const validID = /^([\w\\.]{0,40})$/
+      const result = validID.test(this.MinuteCode)
+      if (this.MinuteCode.length >= 1) {
+        return result
+      }
+      return null
+    },
+  },
+  methods: {
+    onClickAddNewButton() {
+      this.$router.push({ name: 'warehouses-exchange-damaged-goods-create' })
+    },
+    onClickUpdateButton() {
+      this.$router.push({ name: 'archive-changeProductsUpdate' })
+    },
   },
 }
 </script>
