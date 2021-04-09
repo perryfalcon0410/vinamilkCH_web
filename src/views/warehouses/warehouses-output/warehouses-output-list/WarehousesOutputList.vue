5cd876c584773a6a5771a0e78b51ce358f4a9072
trunglt <trunglt@imt-soft.com>
2021-04-09 15:10:26 +0700

    update print pdf

--- src/views/warehouses/warehouses-output/warehouses-output-list/WarehousesOutputList.vue
+++ src/views/warehouses/warehouses-output/warehouses-output-list/WarehousesOutputList.vue
@@ -217,7 +217,28 @@
             </div>
           </template>
           <!-- END - Row -->
+          <!-- START - Column filter -->
+          <template
+            slot="column-filter"
+            slot-scope="props"
+          >
+            <b-row
+              v-if="props.column.field === 'Quantity'"
+              class="mx-0"
+              align-h="end"
+            >
+              6800
+            </b-row>
 
+            <b-row
+              v-else-if="props.column.field === 'Price'"
+              class="mx-0"
+              align-h="end"
+            >
+              250.300.000
+            </b-row>
+          </template>
+          <!-- START - Column filter -->
         </vue-good-table>
       </b-col>
       <!-- END - Table -->
@@ -315,11 +336,6 @@ export default {
           field: 'Feature',
           sortable: false,
         },
-        {
-          label: 'Receipt Type',
-          field: 'ArchiveExportReceiptType',
-          hidden: true,
-        },
       ],
     }
   },
@@ -389,12 +405,9 @@ export default {
     onClickUpdateButton() {
       this.$router.push({ name: 'warehouses-output-update' })
     },
-<<<<<<< HEAD
-=======
     onClickPrintButton(itemIndex) {
       const params = {
-        shopId: 1,
-        transCode: this.receiptExports[itemIndex].ArchiveExportID,
+        transCode: this.receiptExports[itemIndex].Id,
       }
       this.PRINT_RECEIPTEXPORT_ACTION(params)
     },
@@ -404,7 +417,6 @@ export default {
     // navigateToUpdate() {
     //   this.$router.push({ name: 'sales-customers-update' })
     // },
->>>>>>> print pdf
   },
 }
 </script>
