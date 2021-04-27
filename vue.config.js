90ad96aaa39e8f0542c19bb7cd55bbec1d1d0f5a
Sang Pham <sangpdh@imt-soft.com>
2021-04-27 17:25:41 +0700

    - update public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? `/${process.env.VUE_APP_BASE_HREF}/`
+    : '/',
   css: {
     loaderOptions: {
       sass: {
