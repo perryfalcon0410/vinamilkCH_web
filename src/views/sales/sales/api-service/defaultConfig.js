bd611c28f80bd0c139901d73bbe8764975f1cfd5
Vy Doan <vydoan@imt-soft.com>
2021-05-10 14:34:01 +0700

    - Update code defaultConfig of product

--- src/views/sales/sales/api-service/defaultConfig.js
+++ src/views/sales/sales/api-service/defaultConfig.js
@@ -2,9 +2,10 @@ const PROMOTION = '/promotions'
 const VOUCHERS = '/vouchers'
 const SALES = '/sales'
 const ONLINE_ORDERS = '/online-orders'
+const PRODUCT = '/products'
 
 export const getVouchersEndpoint = `${PROMOTION}/${VOUCHERS}`
 export const getOnlineOrdersEndpoint = `${SALES}/${ONLINE_ORDERS}`
-export const getProductsEndpoint = `${SALES}/products`
-export const getProductInfosEndpoint = `${SALES}/products/product-infos`
-export const getProductsTopSaleEndpoint = `${SALES}/products/top-sale`
+export const getProductsEndpoint = `${SALES}${PRODUCT}`
+export const getProductInfosEndpoint = `${SALES}${PRODUCT}/product-infos`
+export const getProductsTopSaleEndpoint = `${SALES}${PRODUCT}/top-sale`
