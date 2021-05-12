4c18276134768ccb4cf03389101a21dd53c2510a
Sang Bui <sangbt@imt-soft.com>
2021-05-12 13:49:52 +0700

    + remove pagnigation @db in purchase

--- src/@db/purchase.js
+++ src/@db/purchase.js
@@ -3,13 +3,4 @@ export default {
     { type: 'A', value: 'Đơn hàng đề nghị' },
     { type: 'B', value: 'Đơn hàng thủ công' },
   ],
-  pagination: [
-    20,
-    40,
-    60,
-    80,
-    100,
-    200,
-    500,
-  ],
 }
