3f5163e20aca44e1e30c447f0cb1acc819a24f50
Vy Doan <vydoan@imt-soft.com>
2021-08-11 08:54:23 +0700

    - Update license_url JSPrintManager

--- src/views/auth/printer-configuration-modal/PrinterConfigurationModal.vue
+++ src/views/auth/printer-configuration-modal/PrinterConfigurationModal.vue
@@ -216,7 +216,7 @@ export default {
       }
     },
     jspmWSStatus() {
-      JSPM.JSPrintManager.license_url = `${window.location.origin}/jspm/`
+      JSPM.JSPrintManager.license_url = 'https://neodynamic.com/licenses/jspm/v4/imt-solutions-test'
       JSPM.JSPrintManager.start()
       JSPM.JSPrintManager.auto_reconnect = true
       if (JSPM.JSPrintManager.websocket_status === JSPM.WSStatus.Open) {
