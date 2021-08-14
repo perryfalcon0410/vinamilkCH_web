94b821f22166bb2ea69f85eaf0ac21beaf8ea028
Sang Bui <sangbt@imt-soft.com>
2021-08-14 22:47:57 +0700

    - Fix

--- src/@core/components/print-form/PrintFormReportOutputStore.vue
+++ src/@core/components/print-form/PrintFormReportOutputStore.vue
@@ -941,17 +941,6 @@ export default {
       }
     })
   },
-  watch: {
-    lstAdjustInfo() {
-      this.expAdjust = { ...this.lstAdjustInfo }
-    },
-    lstPoInfo() {
-      this.expPO = { ...this.lstPoInfo }
-    },
-    lstStockInfo() {
-      this.expBorrow = { ...this.lstStockInfo }
-    },
-  },
   updated() {
     JSPM.JSPrintManager.auto_reconnect = true
     const printerName = this.printerOptions.reportPrinterName
