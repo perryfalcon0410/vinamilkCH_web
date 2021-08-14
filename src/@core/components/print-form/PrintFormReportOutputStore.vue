4df1089c13f64bf07784d5711a6bbae92008d4d8
Sang Bui <sangbt@imt-soft.com>
2021-08-14 22:47:57 +0700

    + fix shot_output print form

--- src/@core/components/print-form/PrintFormReportOutputStore.vue
+++ src/@core/components/print-form/PrintFormReportOutputStore.vue
@@ -920,6 +920,17 @@ export default {
       this.expBorrow = { ...this.lstStockInfo }
     },
   },
+  watch: {
+    lstAdjustInfo() {
+      this.expAdjust = { ...this.lstAdjustInfo }
+    },
+    lstPoInfo() {
+      this.expPO = { ...this.lstPoInfo }
+    },
+    lstStockInfo() {
+      this.expBorrow = { ...this.lstStockInfo }
+    },
+  },
   updated() {
     const printerName = this.printerOptions.reportPrinterName
     const element = document.getElementById('rp-output-store')
