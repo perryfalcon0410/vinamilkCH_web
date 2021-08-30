a9e2fc6002771316b178af9f90c6150cacce6166
Vinh Le <vinhln@imt-soft.com>
2021-08-30 23:56:27 +0700

    - fix disable change bill when not permission

--- src/views/sales/sales/sales-list/SalesList.vue
+++ src/views/sales/sales/sales-list/SalesList.vue
@@ -990,12 +990,12 @@ export default {
           this.currentOrderNumber.onlineOrderId = bill.orderId
           this.currentOrderNumber.orderNumber = bill.orderNumber
           this.currentOrderNumber.note = bill.note
+          this.onlineOrderId = bill.orderNumber
 
-          if (bill.id === this.billFirst) {
+          if (this.onlineOrderId === undefined) {
             this.onlineOrderId = null
-          } else {
-            this.onlineOrderId = bill.orderNumber
           }
+
           return {
             ...bill,
             active: true,
