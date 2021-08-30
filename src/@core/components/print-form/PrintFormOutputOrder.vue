6f242ee1ee6a027798ebb55aa0af650281e06c10
Thoai La <thoail@imt-soft.com>
2021-08-30 14:45:15 +0700

    - Update print form output

--- src/@core/components/print-form/PrintFormOutputOrder.vue
+++ src/@core/components/print-form/PrintFormOutputOrder.vue
@@ -259,11 +259,15 @@
 import { mapGetters, mapActions } from 'vuex'
 import {
   hostName,
-  printActions,
   jspmCheckStatus,
+  jsPdfPrint,
 } from '@core/utils/filter'
+import jsPDF from 'jspdf'
+// eslint-disable-next-line no-unused-vars
+import autoTable from 'jspdf-autotable'
+import { myFontNormal } from '@/@core/libs/Arimo-Regular'
+import { myFontBold } from '@/@core/libs/Arimo-Bold'
 import JSPM from 'jsprintmanager'
-import toasts from '@/@core/utils/toasts/toasts'
 import {
   WAREHOUSES_OUTPUT,
   // Getters
@@ -283,6 +287,8 @@ export default {
         formId: 1,
         ctrlId: 1,
       },
+      bodyData: [],
+      count: 1,
     }
   },
 
@@ -359,35 +365,311 @@ export default {
   },
   updated() {
     JSPM.JSPrintManager.auto_reconnect = true
-    const printerName = this.printerOptions.reportPrinterName
-    if (printerName === '' || printerName === null) {
-      toasts.error('Không tìm thấy tên máy in. Bạn hãy vào cấu hình máy in')
-    } else {
-      JSPM.JSPrintManager.start()
-      for (let i = 0; i < 3; i += 1) {
-        if (JSPM.JSPrintManager.websocket_status === JSPM.WSStatus.Open && i < 3) {
-          const element = document.getElementById('warehouses-output')
-          const options = {
-            fileName: 'Kho xuat hang',
-            format: 'a4',
-            // orientation: 'landscape',
-            // rotate: 'Rot90',
-            pageSizing: 'Fit',
-            scale: 2.5,
-            isPaging: true,
-            x: 1.1,
-          }
-          if (jspmCheckStatus()) {
-            printActions(element, printerName, options)
-          }
-        } else if (JSPM.JSPrintManager.websocket_status === JSPM.WSStatus.Closed && i === 2) {
-          toasts.error('Bạn hãy vào cấu hình máy in trước khi in.')
-        }
-      }
+    this.printerName = this.printerOptions.reportPrinterName
+
+    // eslint-disable-next-line new-cap
+    const pdf = new jsPDF('p', 'mm', 'a4')
+    // START - add font family
+    pdf.addFileToVFS('Ario-Regular.ttf', myFontNormal)
+    pdf.addFileToVFS('Ario-Bold.ttf', myFontBold)
+    pdf.addFont('Ario-Regular.ttf', 'Ario-Regular', 'normal')
+    pdf.addFont('Ario-Bold.ttf', 'Ario-Bold', 'normal')
+    pdf.setFont('Ario-Bold')
+    this.createTable1(pdf)
+    this.createTable2(pdf)
+
+    if (this.inputOrderSaleProductsData && this.inputOrderSaleProductsData.length > 0) {
+      this.inputOrderSaleProductsData.forEach(data => {
+        const row = [
+          { content: 'Ngành hàng:', colSpan: 2, styles: { lineWidth: 0 } },
+          { content: `${data.type}`, styles: { font: 'Ario-Bold', lineWidth: 0 } },
+          { content: 'Tổng SL :', styles: { lineWidth: 0 } },
+          { content: `${this.$formatNumberToLocale(data.totalQuantity)}`, styles: { font: 'Ario-Bold', halign: 'right', lineWidth: 0 } },
+          { content: 'T.Tiền :', styles: { halign: 'right', lineWidth: 0 } },
+          { content: `${this.$formatNumberToLocale(this.inputOrderInfoData.transType === 0 ? data.totalPriceNotVat : data.totalPrice)}`, styles: { font: 'Ario-Bold', halign: 'right', lineWidth: 0 } },
+        ]
+        this.bodyData.push(row)
+        const title = [
+          { content: 'STT', dataKey: 'STT', styles: { font: 'Ario-Bold' } },
+          {
+            content: 'Mã SP', dataKey: 'Mã SP', cellWidth: 35, styles: { font: 'Ario-Bold' },
+          },
+          {
+            content: 'Tên SP', dataKey: 'Tên SP', cellWidth: 60, styles: { font: 'Ario-Bold' },
+          },
+          {
+            content: 'ĐVT', dataKey: 'ĐVT', cellWidth: 15, styles: { font: 'Ario-Bold' },
+          },
+          {
+            content: 'SL', dataKey: 'SL', cellWidth: 15, styles: { font: 'Ario-Bold' },
+          },
+          {
+            content: 'Giá', dataKey: 'Giá', cellWidth: 20, styles: { font: 'Ario-Bold' },
+          },
+          {
+            content: 'T.Tiền', dataKey: 'T.Tiền', cellWidth: 30, styles: { font: 'Ario-Bold' },
+          },
+        ]
+        this.bodyData.push(title)
+        data.products.forEach(pro => {
+          this.bodyData.push([
+            { content: `${this.count}`, styles: { cellWidth: 10 } },
+            { content: `${pro.productCode}`, styles: { cellWidth: 25 } },
+            { content: `${pro.productName}`, styles: { cellWidth: 60 } },
+            { content: `${pro.unit}`, styles: { cellWidth: 20, halign: 'center' } },
+            { content: `${pro.quantity}`, styles: { cellWidth: 15, halign: 'right' } },
+            { content: `${this.$formatNumberToLocale(pro.price)}`, styles: { cellWidth: 30, halign: 'right' } },
+            { content: `${this.$formatNumberToLocale(pro.totalPrice)}`, styles: { cellWidth: 30, halign: 'right' } },
+          ])
+          this.count += 1
+        })
+        pdf.autoTable({
+          theme: 'grid',
+          startY: pdf.previousAutoTable.finalY + 5,
+          margin: {
+            right: 10,
+            left: 10,
+          },
+          styles: {
+            font: 'Ario-Regular',
+            fontSize: 10,
+            textColor: 'black',
+          },
+          didDrawCell: key => {
+            if (key.section === 'body' && key.row.index === 0) {
+              pdf.setDrawColor('black')
+              pdf.setLineWidth(0.1)
+              pdf.line(key.cell.x, key.cursor.y, key.cell.x + key.cell.width, key.cursor.y)
+            }
+            if (key.section === 'body' && key.row.index === 1) {
+              pdf.setDrawColor('black')
+              pdf.setLineWidth(0.1)
+              pdf.line(key.cell.x, key.cursor.y, key.cell.x + key.cell.width, key.cursor.y)
+              if (key.column.index === 1 || key.column.index === 2 || key.column.index === 3 || key.column.index === 4 || key.column.index === 5 || key.column.index === 6) {
+                pdf.line(key.cell.x, key.cell.y + key.cell.height, key.cell.x, key.cell.y)
+              }
+            }
+            if (key.section === 'body' && key.row.index === 2) {
+              pdf.setDrawColor('black')
+              pdf.setLineWidth(0.1)
+              pdf.line(key.cell.x, key.cursor.y, key.cell.x + key.cell.width, key.cursor.y)
+            }
+            if (key.section === 'body' && key.column.index === 0) {
+              pdf.setDrawColor('black')
+              pdf.setLineWidth(0.1)
+              pdf.line(key.cell.x, key.cell.y + key.cell.height, key.cell.x, key.cell.y)
+            }
+            if (key.section === 'body' && key.column.index === 6) {
+              pdf.setDrawColor('black')
+              pdf.setLineWidth(0.1)
+              pdf.line(key.cell.x + key.cell.width, key.cell.y + key.cell.height, key.cell.x + key.cell.width, key.cell.y)
+            }
+            if (key.section === 'body' && key.row.index === key.table.body.length - 1) {
+              pdf.setDrawColor('black')
+              pdf.setLineWidth(0.1)
+              pdf.line(key.cell.x, key.cell.y + key.cell.height, key.cell.x + key.cell.width, key.cell.y + key.cell.height)
+            }
+          },
+          body: [...this.bodyData],
+        })
+        this.bodyData = []
+      })
+    }
+
+    const startYNote = pdf.previousAutoTable.finalY
+
+    this.createTable3(pdf)
+    pdf.setFontSize(10)
+    pdf.setFont('Ario-Regular')
+    pdf.text(`Ghi chú: ${this.inputOrderInfoData.note}`, 10, startYNote + 24, {
+      maxWidth: 60,
+    })
+    pdf.setFontSize(10)
+    pdf.setFont('Ario-Regular')
+    pdf.text('NGƯỜI NHẬP HÀNG', 25, pdf.previousAutoTable.finalY + 30)
+    pdf.text('NGƯỜI XUẤT HÀNG', 163, pdf.previousAutoTable.finalY + 30)
+
+    for (let j = 1; j <= pdf.internal.getNumberOfPages(); j += 1) {
+      pdf.setPage(j)
+      pdf.text(`${j} / ${pdf.internal.getNumberOfPages()}`, pdf.internal.pageSize.getWidth() - 10, pdf.internal.pageSize.getHeight() - 10)
+    }
+
+    const options = {
+      fileName: 'Bao_cao_xuat_hang',
+      pageSizing: 'Fit',
+    }
+    if (jspmCheckStatus()) {
+      jsPdfPrint(pdf.output('datauristring'), this.printerName, options)
     }
   },
   methods: {
     ...mapActions(PRINTERCONFIG, [GET_PRINTER_CLIENT_ACTIONS]),
+    // Table 1
+    createTable1(pdf) {
+      const cellPadding = { cellPadding: 0.5 }
+      const valignDef = { valign: 'middle' }
+      const styleDef = { ...valignDef, ...cellPadding }
+      pdf.autoTable({
+        startY: 5,
+        theme: 'plain',
+        margin: {
+          right: 5,
+          left: 5,
+        },
+        styles: {
+          font: 'Ario-Regular',
+          fontSize: 10,
+          textColor: 'black',
+        },
+        body: [
+        // title
+          [
+            { content: '', styles: { cellWidth: 20, ...cellPadding } },
+            {
+              content: `${this.inputOrderShopData.shopName}`,
+              styles: {
+                cellWidth: 70, font: 'Ario-Bold', fontSize: 12, ...styleDef,
+              },
+            },
+            { content: '', styles: { cellWidth: 10, ...cellPadding } },
+            {
+              content: 'CÔNG TY CP SỮA VIỆT NAM',
+              styles: {
+                font: 'Ario-Bold', fontSize: 15, ...styleDef,
+              },
+            },
+          ],
+          // title
+          [
+            { content: '', styles: { cellWidth: 20, ...cellPadding } },
+            {
+              content: `${this.inputOrderShopData.address}`, styles: { ...styleDef },
+            },
+            { content: '', styles: { cellWidth: 10, ...cellPadding } },
+            {
+              content: 'Số 10 Tân Trào, Phường Tân Phú, Q7, Tp.HCM', styles: { ...styleDef },
+            },
+          ],
+          [
+            { content: '', styles: { cellWidth: 20, ...cellPadding } },
+            {
+              content: `Tel: ${this.inputOrderShopData.phone} - Fax: ${this.inputOrderShopData.fax}`, styles: { ...cellPadding },
+            },
+            { content: '', styles: { cellWidth: 10, ...cellPadding } },
+            {
+              content: 'Tel: (84.8) 54 155 555 - Fax: (84.8) 54 161 226', styles: { ...cellPadding },
+            },
+          ],
+        ],
+      })
+    },
+    // Table 1
+
+    // Table 2
+    createTable2(pdf) {
+      const cellPadding = { cellPadding: 0.5 }
+      const valignDef = { valign: 'middle' }
+      const styleDef = { ...valignDef, ...cellPadding }
+      pdf.autoTable({
+        startY: pdf.previousAutoTable.finalY,
+        theme: 'plain',
+        margin: {
+          right: 10,
+          left: 10,
+        },
+        Padding: 0,
+        styles: {
+          font: 'Ario-Regular',
+          fontSize: 10,
+          textColor: 'black',
+        },
+        didDrawCell: data => {
+          if (data.section === 'body' && data.row.index === 0) {
+            pdf.setDrawColor('black')
+            pdf.setLineWidth(0.1)
+            pdf.line(data.cell.x, data.cursor.y, data.cell.x + data.cell.width, data.cursor.y)
+          }
+        },
+        body: [
+        // title
+          [
+            { content: 'HÓA ĐƠN XUẤT HÀNG', colSpan: 4, styles: { font: 'Ario-Bold', fontSize: 20, cellPadding: { left: 0.5, top: 3, bottom: 2 } } },
+          ],
+          [
+            { content: 'Loại xuất hàng:', styles: { cellWidth: 30, ...styleDef } },
+            {
+              content: `${this.$getInputTypeslabel(this.inputOrderInfoData.transType)}`,
+              styles: {
+                cellWidth: 50, font: 'Ario-Bold', ...styleDef,
+              },
+            },
+            { content: '', styles: { cellWidth: 30, ...styleDef } },
+            { content: '', styles: { ...styleDef } },
+          ],
+          [
+            { content: 'Mã xuất hàng:', styles: { ...styleDef } },
+            { content: `${this.inputOrderInfoData.transCode}`, styles: { font: 'Ario-Bold', ...styleDef } },
+            { content: 'Ngày xuất hàng:', styles: { ...styleDef } },
+            { content: `${this.$formatISOtoVNI(this.inputOrderInfoData.transDate, true)}`, styles: { font: 'Ario-Bold', ...styleDef } },
+          ],
+          [
+            { content: 'Số PO:', styles: { ...styleDef } },
+            { content: `${this.inputOrderInfoData.poNumber}`, styles: { font: 'Ario-Bold', ...styleDef } },
+            { content: 'Số nội bộ:', styles: { ...styleDef } },
+            { content: `${this.inputOrderInfoData.internalNumber}`, styles: { font: 'Ario-Bold', ...styleDef } },
+          ],
+          [
+            { content: 'Số hóa đơn:', styles: { ...styleDef } },
+            { content: `${this.inputOrderInfoData.invoiceNumber}`, styles: { font: 'Ario-Bold', ...styleDef } },
+            { content: 'Ngày hóa đơn:', styles: { ...styleDef } },
+            { content: `${this.$formatISOtoVNI(this.inputOrderInfoData.orderDate)}`, styles: { font: 'Ario-Bold', ...styleDef } },
+          ],
+        ],
+      })
+    },
+    // Table 2
+
+    // Table 3
+    createTable3(pdf) {
+      pdf.autoTable({
+        theme: 'grid',
+        startY: pdf.previousAutoTable.finalY + 10,
+        margin: {
+          right: 10,
+          left: 10,
+        },
+        styles: {
+          font: 'Ario-Bold',
+          fontSize: 10,
+          textColor: 'black',
+        },
+        body: [
+          [
+            {
+              content: 'Tổng SL',
+              styles: { halign: 'right', cellWidth: 112, lineWidth: 0 },
+            },
+            {
+              content: `${this.$formatNumberToLocale(this.inputOrderInfoData.totalQuantity)}`,
+              styles: {
+                halign: 'right', font: 'Ario-Bold', fontSize: 11, cellWidth: 18, lineWidth: 0,
+              },
+            },
+            {
+              content: 'Tổng T.Tiền',
+              styles: { halign: 'right', cellWidth: 29, lineWidth: 0 },
+            },
+            {
+              content: `${this.$formatNumberToLocale(this.inputOrderInfoData.totalPrice)}`,
+              styles: {
+                font: 'Ario-Bold', halign: 'right', fontSize: 11, cellWidth: 31, lineWidth: 0,
+              },
+            },
+          ],
+        ],
+      })
+    },
+    // Table 3
   },
 }
 </script>
