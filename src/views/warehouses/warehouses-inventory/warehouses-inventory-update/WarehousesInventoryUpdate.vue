a439f845219d57394f819a80f565726e1f7500da
Tai Phan <taipt@imt-soft.com>
2021-09-14 15:12:06 +0700

    -fix bug uat: row 237 change event key enter inventory

--- src/views/warehouses/warehouses-inventory/warehouses-inventory-update/WarehousesInventoryUpdate.vue
+++ src/views/warehouses/warehouses-inventory/warehouses-inventory-update/WarehousesInventoryUpdate.vue
@@ -217,7 +217,7 @@
                 @keypress.native="$onlyNumberInput"
                 @keyup.up.native="onClickUpButtonInventoryPacket(props.row.originalIndex)"
                 @keyup.down.native="onClickDownButtonInventoryPacket(props.row.originalIndex)"
-                @keyup.enter.native="onClickDownButtonInventoryPacket(props.row.originalIndex)"
+                @keyup.enter.native="onClickRightButtonInventoryPacket(props.row.originalIndex)"
                 @keyup.left.native="onClickLeftButtonInventoryPacket(props.row.originalIndex)"
                 @keyup.right.native="onClickRightButtonInventoryPacket(props.row.originalIndex)"
               />
@@ -235,7 +235,7 @@
                 @keypress.native="$onlyNumberInput"
                 @keyup.up.native="onClickUpButtonInventoryOdd(props.row.originalIndex)"
                 @keyup.down.native="onClickDownButtonInventoryOdd(props.row.originalIndex)"
-                @keyup.enter.native="onClickDownButtonInventoryOdd(props.row.originalIndex)"
+                @keyup.enter.native="onClickRightButtonInventoryOdd(props.row.originalIndex)"
                 @keyup.left.native="onClickLeftButtonInventoryOdd(props.row.originalIndex)"
                 @keyup.right.native="onClickRightButtonInventoryOdd(props.row.originalIndex)"
               />
