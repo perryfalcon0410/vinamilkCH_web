1127ff0a5a1bfc9d8418c04a25b6535c1531a8ad
Hiếu Phương <phuonglh18@viettel.com.vn>
2022-03-02 09:16:16 +0700

    update api manage PO

--- src/views/purchases/api-service/index.js
+++ src/views/purchases/api-service/index.js
@@ -3,26 +3,26 @@ import { formatURLParams } from '@/@core/utils/utils'
 
 export default {
   getAllPoAuto() {
-    return axios.get('/sales/po-list')
+    return axios.get('/sales/po-auto/po-list')
   },
 
   getSearchPoAuto(args) {
-    return axios.get('/sales/po-search-list', {
+    return axios.get('/sales/po-auto/po-search-list', {
       params: formatURLParams(args),
     })
   },
 
   getPoAutoDetailProduct(args) {
-    return axios.get('/sales/po-detail-product', {
+    return axios.get('/sales/po-auto/po-detail-product', {
       params: formatURLParams(args),
     })
   },
 
   approvePoAUto(args) {
-    return axios.post('/sales/approve-po', args)
+    return axios.post('/sales/po-auto/approve-po', args)
   },
 
   cancelPoAuto(args) {
-    return axios.post('/sales/cancel-po', args)
+    return axios.post('/sales/po-auto/cancel-po', args)
   },
 }
